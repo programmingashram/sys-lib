e11af3e73f2b40a148aa4ce333913c1543e8b401
ayushman76 <avenuedesign768@gmail.com>
2023-04-17 08:25:47 +0530

    added

---
+++ projects/sys-lib/src/lib/sys-card/sys-card-action/sys-card-action.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-sys-card-action',
+  templateUrl: './sys-card-action.component.html',
+  styleUrls: ['./sys-card-action.component.css']
+})
+export class SysCardActionComponent {
+
+}
